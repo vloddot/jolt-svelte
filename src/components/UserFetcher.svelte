185cb1265115c8951913030019ccfd682880b853
vloddot <tamoraa08@gmail.com>
2023-08-09 00:00:23 +0300

    fix(members list): limit to online users only
    feat(controls): edit button that currently does nothing

---
+++ src/components/UserFetcher.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { userCacheKey } from '$lib/fetcher';
+  import { getContext } from 'svelte';
+  import { users } from '$lib/stores';
+  import type { Writable } from 'svelte/store';
+
+  export let id: string;
+
+  const userCache = getContext<Writable<User[]> | null>(userCacheKey);
+  $: user = ($userCache ?? $users)?.find((user) => user._id === id);
+</script>
+
+<slot {user} />
