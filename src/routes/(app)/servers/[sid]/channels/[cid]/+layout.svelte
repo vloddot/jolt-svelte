1ea869a6ca23839d6ccaac3267be8ff8397fd068
vloddot <t.mokhtar.vd@gmail.com>
2023-10-09 23:08:34 +0300

    fix: members list slow on revolt lounge
    just delete the members list :p

--- src/routes/(app)/servers/[sid]/channels/[cid]/+layout.svelte
+++ src/routes/(app)/servers/[sid]/channels/[cid]/+layout.svelte
@@ -1,22 +1,19 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { getContext, setContext } from '$lib/context';
-	import { getDisplayAvatar, getDisplayName } from '$lib/util';
 	import ChannelCategory from '@components/ChannelBar/Category.svelte';
 	import ChannelComponent from '@components/ChannelBar/Channel.svelte';
 	import { selectedChannelIDKey } from '@routes/(app)/context';
-	import { clientKey, settingsKey } from '@routes/context';
+	import { clientKey } from '@routes/context';
 	import { redirect } from '@sveltejs/kit';
 	import { appWindow } from '@tauri-apps/api/window';
 	import { writable } from 'svelte/store';
 	import type { RouteParams } from './$types';
 	import { channelKey } from './context';
 	import { serverKey } from '../../context';
-	import UserIcon from '@components/Icons/UserIcon.svelte';
 	import { onDestroy } from 'svelte';
 
 	const client = getContext(clientKey)!;
-	const settings = getContext(settingsKey)!;
 	const server = getContext(serverKey);
 
 	$: pageParams = $page.params as RouteParams;
@@ -98,38 +95,3 @@
 {/if}
 
 <slot />
-
-{#if $server != undefined}
-	<div class="members-list-container">
-		{#await client.api.fetchMembers($server._id) then response}
-			{#each response.members as member}
-				{@const user =
-					response.users.find(({ _id }) => _id == member._id.user) ??
-					client.api.fetchUser(member._id.user)}
-
-				{#await user then user}
-					{@const name = getDisplayName(user, member)}
-					<div>
-						{#if $settings['jolt:low-data-mode']}
-							<UserIcon />
-						{:else}
-							{@const avatar = getDisplayAvatar(user, member)}
-							{#if typeof avatar == 'string'}
-								<img class="cover" src={avatar} alt={name} />
-							{/if}
-						{/if}
-
-						{name}
-					</div>
-				{/await}
-			{/each}
-		{/await}
-	</div>
-{/if}
-
-<style lang="scss">
-	img.cover {
-		width: 24px;
-		height: 24px;
-	}
-</style>
