a38131dcccea0b60f9f4d46fd6b5bc26cfd1183d
vloddot <t.mokhtar.vd@gmail.com>
2023-11-01 13:00:28 +0200

    Updates

--- _app/immutable/chunks/navigation.2ea4b366.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./singletons.1e1430be.js";const e=o("goto");export{e as g};
---
+++ _app/immutable/chunks/util.d5ec8d73.js
@@ -0,0 +1,1 @@
+const p=30,d=10;function r(a,e){return e!=null?new Map(a.map(n=>[e(n._id),n])):new Map(a.map(n=>[n._id,n]))}export{d as P,p as a,r as m};
