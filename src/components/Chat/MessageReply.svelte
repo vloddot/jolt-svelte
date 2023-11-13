79db08c0745afe887e7f358a0cef5730a3c5c397
vloddot <t.mokhtar.vd@gmail.com>
2023-11-13 20:15:52 +0200

    fix: messages handling member and message display

--- src/components/Chat/MessageReply.svelte
+++ src/components/Chat/MessageReply.svelte
@@ -3,7 +3,7 @@
 	import { getContext } from '$lib/context';
 	import { getDisplayAvatar, getDisplayName } from '$lib/util';
 	import { clientKey, settingsKey } from '@routes/context';
-	import { getUser, messagesKey, usersKey } from '.';
+	import { getUser, membersKey, messagesKey, usersKey } from '.';
 	import GenericUserCircleIcon from '@components/Icons/GenericUserCircleIcon.svelte';
 	import { selectedChannelKey, selectedServerKey } from '@routes/(app)/context';
 
@@ -13,6 +13,7 @@
 	const server = getContext(selectedServerKey);
 	const messages = getContext(messagesKey);
 	const users = getContext(usersKey);
+	const members = getContext(membersKey);
 
 	export let id: string;
 </script>
@@ -23,38 +24,38 @@
 		: `servers/${$server?._id}/channels/${$channel?._id}/${id}`}"
 >
 	{#if $channel != undefined}
-		{#await $messages?.find(({ _id: mid }) => id == mid) then message}
-			{#if message == undefined}
-				Could not resolve message.
-			{:else}
-				{#if !$settings['jolt:low-data-mode']}
-					{#await getUser( $users ?? client.cache.users, message.author, (id) => client.fetchUser(id) )}
+		{@const message = $messages?.find(({ _id: mid }) => id == mid)}
+		{#if message == undefined}
+			Could not resolve message.
+		{:else}
+			{#if !$settings['jolt:low-data-mode']}
+				{#await getUser($users ?? client.cache.users, message.author, (id) => client.fetchUser(id))}
+					<GenericUserCircleIcon />
+
+					&lt;Unknown User&gt;
+				{:then author}
+					{#if author != undefined}
+						{@const member = $members?.find((member) => member._id.user == author._id)}
+						{@const displayName = getDisplayName(author, member, message)}
+						<img
+							class="cover"
+							src={getDisplayAvatar(author, member, message)}
+							alt={displayName}
+							width="24px"
+							height="24px"
+						/>
+						{displayName}
+					{:else}
 						<GenericUserCircleIcon />
 
 						&lt;Unknown User&gt;
-					{:then author}
-						{#if author != undefined}
-							{@const displayName = getDisplayName(author)}
-							<img
-								class="cover"
-								src={getDisplayAvatar(author)}
-								alt={displayName}
-								width="24px"
-								height="24px"
-							/>
-							{displayName}
-						{:else}
-							<GenericUserCircleIcon />
-
-							&lt;Unknown User&gt;
-						{/if}
-					{/await}
-				{/if}
-				{#if message.content != undefined}
-					<span class="message-content">{message.content}</span>
-				{/if}
+					{/if}
+				{/await}
 			{/if}
-		{/await}
+			{#if message.content != undefined}
+				<span class="message-content">{message.content}</span>
+			{/if}
+		{/if}
 	{/if}
 </a>
 
